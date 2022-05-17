75a6f38995827ffb4813d2631f93bf519f1a9743
Alexandr Kurkin <2012stalker@gmail.com>
2022-05-17 11:26:43 +0300

    changes

--- scripts/callback.js
+++ scripts/callback.js
@@ -472,15 +472,24 @@ function phoneCallBackWidget(env) {
     '\t\t}\n' +
     '\t</style>'
 
-  function loadScript(url) {
-    const script = document.createElement('script')
-    script.type = 'text/javascript'
-    script.src = url
-    script.async = false
-    document.head.appendChild(script)
-    return new Promise((resolve) => {
-      script.onreadystatechange = resolve()
-      script.onload = resolve()
+  function loadScript(src) {
+    return new Promise(function (resolve, reject) {
+      const s = document.createElement('script')
+      let r = false
+      s.type = 'text/javascript'
+      s.src = src
+      s.async = true
+      s.onerror = function (err) {
+        reject(err, s)
+      }
+      s.onload = s.onreadystatechange = function () {
+        if (!r && (!this.readyState || this.readyState == 'complete')) {
+          r = true
+          resolve()
+        }
+      }
+      const t = document.getElementsByTagName('script')[0]
+      t.parentElement.insertBefore(s, t)
     })
   }
 
